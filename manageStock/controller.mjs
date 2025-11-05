2cd68146a063566112d006237307f658c287f0c5
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-05 19:53:21 +0530

    update getAllUsers controller

--- manageStock/controller.mjs
+++ manageStock/controller.mjs
@@ -1,22 +1,29 @@
+import { en } from 'zod/v4/locales';
+import { ServerError } from '../error.mjs';
 import { DB_ERR_CODES, prisma } from '../prisma/db.mjs';
 
 const getProducts = async (req, res, next) => {
   // const products = await prisma.product.findMany();
 
-  const products = await prisma.product.findMany({
-    include: {
-      inventory: {
-        select: { quantity: true },
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        inventory: {
+          select: { quantity: true },
+        },
+        entry: {
+          orderBy: { entryDate: 'desc' },
+          take: 1, // get only the latest entry (for latest price)
+          select: { price: true, quantity: true },
+        },
       },
-      entry: {
-        orderBy: { entryDate: 'desc' },
-        take: 1, // get only the latest entry (for latest price)
-        select: { price: true, quantity: true },
-      },
-    },
-  });
+    });
 
-  res.json({ message: 'Products retrieved successfully', products });
+    res.json({ message: 'Products retrieved successfully', products });
+  } catch (error) {
+    console.error(error);
+    throw new ServerError(500, 'unable to fetch products');
+  }
 };
 
 const getAllInventory = async (req, res, next) => {
@@ -41,153 +48,161 @@ const getAllInventory = async (req, res, next) => {
     res.json({ message: 'All inventories', inventories });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error fetching inventories', error });
+    throw new ServerError(500, 'unable to fetch inventories');
   }
 };
 
 const getAllUsers = async (req, res, next) => {
-  const allUsers = await prisma.user.findMany();
-
-  res.json({ message: 'All Team Members', allUsers });
-};
-
-// const addProduct = async (req, res, next) => {
-//   try {
-//     const { name, skuId, description, minQuantity, category } = req.body;
-
-//     // Step 1: Create the Product
-//     const product = await prisma.product.create({
-//       data: {
-//         name,
-//         skuId,
-//         description,
-//         minQuantity,
-//         category,
-//       },
-//     });
-
-//     // Step 2: Automatically create Inventory record for this product
-//     const inventory = await prisma.inventory.create({
-//       data: {
-//         productId: product.id,
-//         quantity: 0, // default quantity
-//         location: null, // or set default if you have one
-//       },
-//     });
-
-//     console.log({ product, inventory });
-
-//     // Step 3: Respond
-//     res.json({
-//       message: 'Product and inventory created successfully',
-//       product,
-//       inventory,
-//     });
-//   } catch (error) {
-//     console.error('Error adding product:', error);
-//     next(error);
-//   }
-// };
-
-const addProduct = async (req, res, next) => {
   try {
-    const { name, price, skuId } = req.body;
-
-    //Check karo ki same SKU already exist to nhi karta
-
-    const existingProduct = await prisma.product.findUnique({
-      where: { skuId: skuId },
-    });
-    if (existingProduct) {
-      return res.status(400).json({
-        massage: 'Product with this SKU already exists!',
-      });
-    }
-    // Agar unique hai, naya product create karo
-    const newProduct = await prisma.product.create({
-      data: {
-        name,
-        price,
-        skuId,
+    const allUsers = await prisma.user.findMany({
+      include: {
+        _count: {
+          select: {
+            entry: true,
+            exit: true,
+          },
+        },
       },
     });
 
-    res.status(200).json({
-      massage: 'Product added successfully',
-      product: newProduct,
-    });
+    res.json({ message: 'All Team Members', allUsers });
   } catch (error) {
-    console.error('Error in addProduct:', error);
-    res
-      .status(500)
-      .json({ massage: 'Internal Server Error', error: error.massage });
+    console.log(error);
+    throw new ServerError(500, 'unable to fetch users');
   }
 };
 
-// const productEntry = async (req, res, next) => {
-//   const { productId, quantity, price, notes, addedBy } = req.body;
-
-//   console.log(req.body); // ✅ check these are not undefined
-
-//   // When adding an entry
-//   const newEntry = await prisma.$transaction(async (tx) => {
-//     await tx.entry.create({
-//       data: { productId, quantity, price, notes, addedBy },
-//     });
-
-//     await tx.inventory.upsert({
-//       where: { productId: productId },
-//       update: { quantity: { increment: quantity } },
-//       create: { productId: productId, quantity: quantity },
-//     });
-//   });
-
-//   res.json({ message: 'Product entry recorded successfully', newEntry });
-// };
-
-const productEntry = async (req, res, next) => {
+const getAllEntries = async (req, res, next) => {
   try {
-    const { productId, quantity, price, notes, addedBy } = res.body;
-    //Phale check karo ki product exitstnhi hai
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 7;
+    const skip = (page - 1) * limit;
 
-    const product = await prisma.product.findMany({
-      where: { id: productId },
+    const [entries, totalCount] = await Promise.all([
+      prisma.entry.findMany({
+        include: {
+          product: { select: { name: true } },
+          user: { select: { id: true, name: true } },
+        },
+        orderBy: { entryDate: 'desc' },
+        skip,
+        take: limit,
+      }),
+      prisma.entry.count(),
+    ]);
+
+    res.json({
+      message: 'Entries fetched successfully',
+      data: entries,
+      pagination: {
+        totalItems: totalCount,
+        currentPage: page,
+        totalPages: Math.ceil(totalCount / limit),
+        limit,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    next(new ServerError(500, 'Unable to fetch entries'));
+  }
+};
+
+const getAllExits = async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 7;
+    const skip = (page - 1) * limit;
+
+    const [exits, totalCount] = await Promise.all([
+      prisma.exit.findMany({
+        include: {
+          product: { select: { name: true } },
+          user: { select: { id: true, name: true } },
+        },
+        orderBy: { exitDate: 'desc' },
+        skip,
+        take: limit,
+      }),
+      prisma.exit.count(),
+    ]);
+
+    res.json({
+      message: 'Exits fetched successfully',
+      data: exits,
+      pagination: {
+        totalItems: totalCount,
+        currentPage: page,
+        totalPages: Math.ceil(totalCount / limit),
+        limit,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    next(new ServerError(500, 'Unable to fetch exits'));
+  }
+};
+
+const addProduct = async (req, res, next) => {
+  try {
+    const { name, skuId, description, minQuantity, category } = req.body;
+
+    // Step 1: Create the Product
+    const product = await prisma.product.create({
+      data: {
+        name,
+        skuId,
+        description,
+        minQuantity,
+        category,
+      },
     });
 
-    if (!product) {
-      return res.status(400).json({
-        massage: `Product with id ${productId} does not exist!`,
-      });
-    }
+    // Step 2: Automatically create Inventory record for this product
+    const inventory = await prisma.inventory.create({
+      data: {
+        productId: product.id,
+        quantity: 0, // default quantity
+        location: null, // or set default if you have one
+      },
+    });
 
-    //Transacton start karo entry create + inventory update
+    console.log({ product, inventory });
 
-    const result = await prisma.$transaction(async (tx) => {
-      // Entry create karo
+    // Step 3: Respond
+    res.json({
+      message: 'Product and inventory created successfully',
+      product,
+      inventory,
+    });
+  } catch (error) {
+    console.error('Error adding product:', error);
+    next(error);
+  }
+};
 
-      const newEntry = await tx.entry.create({
+const productEntry = async (req, res, next) => {
+  const { productId, quantity, price, notes, addedBy } = req.body;
+
+  console.log(req.body); // ✅ check these are not undefined
+
+  // When adding an entry
+  try {
+    const newEntry = await prisma.$transaction(async (tx) => {
+      await tx.entry.create({
         data: { productId, quantity, price, notes, addedBy },
       });
 
-      //Inventory update karo (upsert)
-
       await tx.inventory.upsert({
-        where: { productId },
-        update: { quqntity: { incrememt: quantity } },
-        create: { productId, quantity },
+        where: { productId: productId },
+        update: { quantity: { increment: quantity } },
+        create: { productId: productId, quantity: quantity },
       });
-      return newEntry;
     });
 
-    res.status(200).json({
-      massage: 'Product entry recorded successfully',
-      entry: result,
-    });
+    res.json({ message: 'Product entry recorded successfully', newEntry });
   } catch (error) {
-    console.error('Error in productEntry:', error);
-    res.status(500).json({
-      error: error.massage,
-    });
+    console.log(error);
+    throw new ServerError(500, 'unable to record product entry');
   }
 };
 
@@ -196,35 +211,42 @@ const productExit = async (req, res, next) => {
 
   console.log(req.body); // ✅ check these are not undefined
 
-  const exitItem = await prisma.$transaction(async (tx) => {
-    const inventory = await tx.inventory.findUnique({
-      where: { productId: productId },
-      select: { quantity: true },
+  try {
+    const exitItem = await prisma.$transaction(async (tx) => {
+      const inventory = await tx.inventory.findUnique({
+        where: { productId: productId },
+        select: { quantity: true },
+      });
+
+      if (!inventory || inventory.quantity < quantity) {
+        throw new Error('Not enough stock to remove');
+      }
+
+      const exit = await tx.exit.create({
+        data: { productId, quantity, price, reason, removedBy },
+      });
+
+      await tx.inventory.update({
+        where: { productId: productId },
+        data: { quantity: { decrement: quantity } },
+      });
+
+      return exit;
     });
 
-    if (!inventory || inventory.quantity < quantity) {
-      throw new Error('Not enough stock to remove');
-    }
-
-    const exit = await tx.exit.create({
-      data: { productId, quantity, price, reason, removedBy },
-    });
-
-    await tx.inventory.update({
-      where: { productId: productId },
-      data: { quantity: { decrement: quantity } },
-    });
-
-    return exit;
-  });
-
-  res.json({ message: 'Product exit recorded successfully', exitItem });
+    res.json({ message: 'Product exit recorded successfully', exitItem });
+  } catch (error) {
+    console.log(error);
+    throw new ServerError(500, 'unable to record product exit');
+  }
 };
 
 export {
   getProducts,
   getAllInventory,
   getAllUsers,
+  getAllEntries,
+  getAllExits,
   addProduct,
   productEntry,
   productExit,
