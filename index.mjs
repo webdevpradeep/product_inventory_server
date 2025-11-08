4e4f56e1b0e840cd92197d8a6e540c3fafa1bc4f
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-11-08 19:43:29 +0530

    allow all routes in CORS

--- index.mjs
+++ index.mjs
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ Enable CORS for frontend origin
 app.use(
   cors({
-    origin: 'http://localhost:5173', // allow your frontend URL
+    origin: '*', // allow your frontend URL
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true, // if using cookies or auth headers
   })
